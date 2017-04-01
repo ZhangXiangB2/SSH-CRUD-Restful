701bea8f9162afd09552fb3edb0b18535621b3f8
ZhangXiangB2 <b25459870@gmail.com>
2017-04-02 03:43:41 +0800

    initial commit

---
+++ WEB-INF/classes/com/user/resStutas/model/ResStutasVO.java
@@ -0,0 +1,30 @@
+package com.user.resStutas.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResStutasVO implements java.io.Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String success;
+	private String error;
+	
+	public ResStutasVO(String asuccess, String aerror){
+		this.success = asuccess;
+		this.error = aerror;
+	}
+
+	public String getError() {
+		return error;
+	}
+	public void setError(String aerror) {
+		this.error = aerror;
+	}
+
+	public String getSuccess() {
+		return success;
+	}
+	public void setSuccess(String asuccess) {
+		this.success = asuccess;
+	}
+}
